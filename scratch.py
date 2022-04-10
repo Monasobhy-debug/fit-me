5bb6503a2e4d7aca7b6a791374ac24d34297219e
Monasobhy-debug <nona.lover2045@gmail.com>
2022-04-10 20:31:59 +0200

    Edit 2

--- scratch.py
+++ scratch.py
@@ -1,9 +1,7 @@
 import sqlite3
 import math
-
 import wcag_contrast_ratio as contrast
 from colour import Color
-from pandas import DataFrame
 import pandas as pd
 import numpy as np
 
@@ -21,8 +19,9 @@ def query_db(id):
     pass
 ok_pieces = query_db(3)
 piece_features = query_db(2)
-print(ok_pieces)
-print(piece_features)
+
+# print(ok_pieces)
+# print(piece_features)
 
 def get_score(piece_features, ok_pieces):
     score = 0
@@ -35,39 +34,48 @@ def get_recommender_rules(piece_features, ok_pieces):
     score=[]
     for rule in range(1,3):
         if(rule==1):
-          print(piece_features.iloc[0])
-          blue = Color("blue").rgb
-          red=Color("red").rgb
-          m=contrast.rgb(blue, red)
-          print(m)
-          print("ok1")
+          print(piece_features["Color"].iloc[0])
+          print(ok_pieces["Color"].iloc[0])
+          c1 = Color("%s"%str(piece_features["Color"].iloc[0])).rgb
+          c2=Color("%s"%str(ok_pieces["Color"].iloc[0])).rgb
+          # c1=Color('black').rgb
+          # c2=Color('white').rgb
+          # print(c2)
+          # print(c1)
+          m=contrast.rgb(c1, c2)
+          # print(m)
+          # print("ok1")
           score.append(math.floor(m))
 
         if(rule==2):
-          print("ok2")
-          z=35
-          score.append(z)
+          print(piece_features["Style"].iloc[0])
+          print(ok_pieces["Style"].iloc[0])
+          s1= str(piece_features["Style"].iloc[0])
+          s2=str(ok_pieces["Style"].iloc[0])
+          if(s1==s2):
+            match = 100
+          else:
+              match=0
+          # print("ok2")
+
+          score.append(match)
 
     print(score)
     return score
 s=get_score(piece_features, ok_pieces)
 print(s)
-#
-#
-# def sort_pieces(ok_pieces, scores):
-#     # we need to implement this
-#     pass
-#
-# def recommender(piece_id: int):
-#     piece_features = query_db(piece_id)
-#     ok_pieces = query_db(piece_id)
-#     scores = []
-#     for i in range(ok_pieces.shape[0]):
-#         scores.append(get_score(piece_features, ok_pieces[i]))
-#     sorted_pieces = sort_pieces(ok_pieces, scores)  # sort ok_pieces according to scores list ...
-#     return sorted_pieces
 
-#def matchColor(c1,c2):
-# c1=pink
-# c2= green
-#
+
+def sort_pieces(ok_pieces, scores):
+    # we need to implement this
+    pass
+
+def recommender(piece_id: int):
+    piece_features = query_db(3)
+    ok_pieces = query_db(2)
+    scores = []
+    for i in range(ok_pieces.shape[0]):
+        scores.append(get_score(piece_features, ok_pieces[i]))
+    sorted_pieces = sort_pieces(ok_pieces, scores)  # sort ok_pieces according to scores list ...
+    return sorted_pieces
+
